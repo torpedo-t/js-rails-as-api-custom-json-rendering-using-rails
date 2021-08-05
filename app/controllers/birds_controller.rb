7d496661ea693c994c2ff62b1a5c74a7b2f16f13
torpedo-t <55634516+torpedo-t@users.noreply.github.com>
2021-08-05 16:44:41 -0500

    Done.

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,19 @@
 class BirdsController < ApplicationController
   def index
-    @birds = Bird.all
-    render json: @birds
+    birds = Bird.all
+    render json: birds, only: [:id, :name, :species]
+    # line of code below is the opposite way of doing the same thing as above
+    # render json: birds, except: [:created_at, :updated_at]
+  end
+
+  def show
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      render json: { id: bird.id, name: bird.name, species: bird.species }
+    else
+      render json: { message: 'Bird not found' }
+    end
+    # this line below does the same thing as the one on line 12, works well for a single hash, but won't work for an array of hashes
+    # render json: bird.slice(:id, :name, :species)
   end
 end
